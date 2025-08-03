103c46ccffbdf587f5590448b775862700d42cb7
worldisconfusion <iamsatvikpandey@gmail.com>
2025-08-03 11:17:49 +0530

    commit

--- Supreme 2.0/Lec16/p1.cpp
+++ Supreme 2.0/Lec16/p1.cpp
@@ -1,7 +1,26 @@
+//Binary Search
+//Array Mustr Be Sorted
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-  cout<<'Hello World!';
+  int arr[10]={1,2,3,4,5,6,7,8,9,10};
+  int brr[11]={1,2,3,4,5,6,7,8,9,10,11};
+  int l=0,r=9;
+  int target,mid;
+  cin>>target;
+  for(;l<r;){
+    mid=l+(r-l)/2;
+    if(target==arr[mid]){
+        cout<<"Target Found!!"<<"\n Index :"<<mid+1;
+        return 0;
+    }
+    else if (target>mid)
+        l=mid+1;
+        else
+        r=mid-1;
+    
+  }
+  cout<<"Target Not Found!!";
   return 0;
 }
